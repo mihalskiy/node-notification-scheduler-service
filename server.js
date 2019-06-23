6674952639bb74c585f587b50cc9cec74a92a61d
m <mihalskiy.rv@gmail.com>
2019-06-23 23:31:00 +0300

    fix package

--- server.js
+++ server.js
@@ -1,11 +1,9 @@
 const express = require('express');
 const logger = require('morgan');
-const cors = require('cors');
 const bodyParser = require('body-parser');
 require('dotenv').config();
 const notification = require('./server/notification/notification');
 const app = express();
-app.use(cors());
 
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "http://localhost:3000");
